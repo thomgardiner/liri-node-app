5adede5e84619b8e94f60e1cc2cd43db6aac5940
Thomas Gardiner <thomgardiner@gmail.com>
2018-05-18 11:05:59 -0600

    twitter command/api call working

--- liri.js
+++ liri.js
@@ -1,17 +1,32 @@
 require("dotenv").config();
 
+let Twitter = require("twitter");
 let keys = require('./keys.js');
 let spotify = keys.spotify;
-let client = keys.twitter;
-
+let client = new Twitter(keys.twitter);
 let command = process.argv[2];
 
-console.log(spotify);
-console.log(client);
+let twitterParams = {
+    screen_name: 'nodejs',
+    count: 5
+};
 
+
+let callTwitter = function(){
+    client.get('statuses/user_timeline', twitterParams, function(error, tweets, response) {
+        if (!error) {
+          for(i=0; i < tweets.length; i++){
+              console.log("---------------");
+              console.log(tweets[i].created_at);
+              console.log(tweets[i].text);
+          }
+        }
+      })
+}
 
 if(command == "my-tweets"){
     //do some stuff
+    callTwitter();
     
 }
 else if(command == "spotify-this-song"){
